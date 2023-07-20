eb12d27241e8d4e4bd901eddc0d8c35d15ff991e
hchiba1984 <h.chiba201103@gmail.com>
2023-07-21 00:35:43 +0900

    upgrade dotnet.

--- src/RedisServerWrapper.Extensions.DependencyInjection/ServiceCollectionExtensionLibrary.cs
+++ src/RedisServerWrapper.Extensions.DependencyInjection/ServiceCollectionExtensionLibrary.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.Configuration;
 using RedisServerWrapper;
+using System;
 
+// ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensionLibrary {
@@ -8,5 +10,5 @@ public static class ServiceCollectionExtensionLibrary {
         services.AddSingleton(new RedisServer(redisSettings));
 
     public static RedisSettings GetRedisSettings(this IConfiguration configuration) =>
-        configuration.GetSection(nameof(RedisSettings)).Get<RedisSettings>();
+        configuration.GetSection(nameof(RedisSettings)).Get<RedisSettings>() ?? throw new InvalidCastException(nameof(configuration));
 }
